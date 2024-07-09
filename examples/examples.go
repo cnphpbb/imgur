bba59db174b70e5c1c9c13bcae71150ed8d088bd
cnphpbb <moqiruyi@gmail.com>
2024-07-09 18:54:37 +0800

    feat(imgur): modity example

--- examples/examples.go
+++ examples/examples.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	i := imgur.LoadFromUrl("https://go.dev/blog/go-brand/logos.jpg").Resize(600, 400)
 	imgur.Canvas(760, 1440, colornames.Deepskyblue).
-		Insert(i, 80, 1440/3).
+		Insert(i, (760-600)/2, (1440-400)/10).
+		Insert(i, (760-600)/2, (1440-400)/2).
+		Insert(i, (760-600)/2, 400*2+(1440-400)/8+5).
 		Save("out.png")
 }
